2e8948f4bc73401ff082b53e459bef8f8fd03b82
joe_hallinan <43181783+G00267801@users.noreply.github.com>
2019-10-21 09:42:22 +0100

    Add files via upload

---
+++ Movie-app/Movie-app-master/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HttpClientModule}from '@angular/common/http';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+MatInputModule,
+MatCardModule,
+MatButtonModule,
+MatToolbarModule,
+MatExpansionModule
+} from '@angular/material';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+import { FormsModule } from '@angular/forms';
+
+//import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+   AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatInputModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
